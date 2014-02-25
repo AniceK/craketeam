83cd3ccd62be0dce89bb7300ab1f30d51c89189e
colas <colas.picard@laposte.net>
2014-02-25 14:34:40 +0100

    ajout du fichier de base pour la classe Grille. Il est vide, ne comportant que la structure de l objet dont une partie est mise en commentaire

---
+++ Codes/Grille.rb
@@ -0,0 +1,24 @@
+#25/02/2014
+#Projet Picross equipe CrakeTeam
+#
+#Ce fichier contient la classe Grille, qui est constituee de Case
+#ici une description de la classe Grille.
+
+class Grille
+
+    #@attribut1
+    #@attribut2
+    
+    #def Grille.ajouter()
+    #end    #marqueur de fin de constructeur
+    
+    #def initialize()
+    #end    #marqueur de fin d initialize
+
+    #def methode1()
+    #end    #marqueur de fin de methode
+
+    #def methode2()
+    #end    #marqueur de fin de methode d instance
+
+end     #marqueur de fin de class
