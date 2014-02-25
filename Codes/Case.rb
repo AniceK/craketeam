f3f43d11443032a1bba730bd6f62df89a64720e9
colas <colas.picard@laposte.net>
2014-02-25 14:32:00 +0100

    ajout du fichier de base pour la classe Case. Il est vide, ne comportant que la structure de l objet dont une partie est mise en commentaire

---
+++ Codes/Case.rb
@@ -0,0 +1,24 @@
+#25/02/2014
+#Projet Picross equipe CrakeTeam
+#
+#Ce fichier contient la classe Case, brique de base du jeu
+#ici une description de la classe Case.
+
+class Case
+
+    #@attribut1
+    #@attribut2
+    
+    #def Case.ajouter()
+    #end    #marqueur de fin de constructeur
+    
+    #def initialize()
+    #end    #marqueur de fin d initialize
+
+    #def methode1()
+    #end    #marqueur de fin de methode
+
+    #def methode2()
+    #end    #marqueur de fin de methode d instance
+
+end     #marqueur de fin de class
