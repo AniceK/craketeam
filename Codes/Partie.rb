a0941ea6f620b7c9efc39ea77c98291f7a1148da
mccartnay <erwan.marchand@orange.fr>
2014-03-13 11:26:08 +0100

    Ajout du squelette de Partie.rb

---
+++ Codes/Partie.rb
@@ -0,0 +1,42 @@
+#13/03/2014
+#Projet Picross équipe CrakeTeam
+#
+#Ce fichier contient la classe Partie, qui est constituée d'une Grille
+#ici une description de la classe Partie.
+
+class Partie
+
+    @grille
+
+	# Constructeur de la classe Partie
+    def Partie.creer()
+    end    #marqueur de fin de constructeur
+
+    def initialize()
+    end    #marqueur de fin d initialize
+
+	# Méthode de test de fin de partie
+	def termine?()
+	end
+
+	# Méthode lançant la partie
+	def lancer()
+	end
+
+	# Méthode permettant de sauvegarder la Partie en court
+	def sauvegarder()
+	end
+
+	# Méthode permettant de charger une Partie déjà créée
+	def charger()
+	end
+
+	# Méthode permettant de supprimer une Partie
+	def supprimer()
+	end
+
+	# Méthode permettant d'initialiser une Partie
+	def initialiser()
+	end
+
+end
