f820498255bc421c0a20af05877df5d1c1ad1f5b
colas <colas.picard@laposte.net>
2014-03-13 10:33:27 +0100

    ajout de Rangee.rb, contenant la classe rangee

---
+++ Codes/Rangee.rb
@@ -0,0 +1,28 @@
+#13/03/2014
+#Projet Picross equipe CrakeTeam
+#
+#Ce fichier contient la classe Rangee, qui est constituee de Case
+#ici une description de la classe Rangee.
+
+class Rangee
+
+    @cases
+
+    attr_reader :Cases
+    
+    def Rangee.ajouter(nbreCase)
+        
+        new(nbreCases)
+    end    #marqueur de fin de constructeur
+    
+    def initialize(nbre)
+
+    end    #marqueur de fin d initialize
+
+    #def methode1()
+    #end    #marqueur de fin de methode
+
+    #def methode2()
+    #end    #marqueur de fin de methode d instance
+
+end     #marqueur de fin de class
