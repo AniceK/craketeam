54f1eaed03bbbf5d0ea97870dbeb9b55d2d4ccfa
mccartnay <erwan.marchand@orange.fr>
2014-03-13 11:26:52 +0100

    Ajout du squelette de Jeu.rb

---
+++ Codes/Jeu.rb
@@ -0,0 +1,31 @@
+#13/03/2014
+#Projet Picross équipe CrakeTeam
+#
+#Ce fichier contient la classe Jeu, qui est constituée d'un Profil et d'une Partie
+#ici une description de la classe Jeu.
+
+class Profil
+
+    @profil
+	@partie
+
+	# Constructeur de la classe Jeu
+    def Jeu.creer()
+    end    #marqueur de fin de constructeur
+
+    def initialize()
+    end    #marqueur de fin d initialize
+
+
+	# Méthode permettant de charger le Profil du joueur
+	def chargerProfil()
+
+	end
+
+
+	# Méthode permettant de récupérer une partie
+	def chargerPartie()
+
+	end
+
+end
