4ce223572f0fde441d6b162aa75f00b41d32ac00
RemiMolette <r.tremblain@gmail.com>
2014-03-18 20:58:44 +0100

    Test personnel

--- Codes/Moteur/test.rb
+++ Codes/Moteur/test.rb
@@ -3,30 +3,9 @@
 # Date : 13/03/14
 # Contenu : Test de déplacement dans les dossiers et test de nom
 #
+require 'fileutils'
+FileUtils.cd('..')
+FileUtils.pwd()
 
-class Test
-  
-  @nom
-  @chemin
-  
-  attr :nom, true
-  attr :chemin, true
-  
-  private_class_method :new
-  
-  def Test.Nouveau(unNom)
-    new(unNom)
-  end
-  
-  def initialize(unNom)
-    @nom = unNom
-  end
-  
-  #Methode de vérification de la présence du nom dans l'arborescence
-  def estPresent?()
-    system("echo #{@nom}")
-  end
-end
-
-j=Test.Nouveau('remi')
-j.estPresent?()
+FileUtils.mkdir('Toto')
+FileUtils.cd('Toto')
