9230eb79a8df7d1ea0e9166de2a6e1609f8e5f31
colas <colas.picard@laposte.net>
2014-04-08 14:11:08 +0200

    modification de Jeu et Regles pour l arborescence

--- Codes/Moteur/Jeu.rb
+++ Codes/Moteur/Jeu.rb
@@ -8,33 +8,24 @@ class Jeu
 
   @profil
   @partie
-  @os       #Variable representant l'os sous lequel le jeu est lancé. 2 signifie Windows, 1 signifie Unix(Mac OS et Linux). Cette variable sert a determiner les commandes a utiliser
 
-	# Constructeur de la classe Jeu
+  # Constructeur de la classe Jeu
     def Jeu.creer()
     end    #marqueur de fin de constructeur
 
     def initialize()
 
 
-        system_name = `uname`
-        if(system_name[1,1] == /[Ww]/) then
-
-            os = 2
-        else
-            os = 1
-        end
-
     end    #marqueur de fin d initialize
 
 
 	# Méthode permettant de charger le Profil du joueur
 	def chargerProfil(unNom)
-	  @profil.charger(unNom)
+	  
 	end
 
 	def ajouterProfil(unNom)
-	  @profil.creer(unNom)
+	  @profil = Profil.creer(unNom)
 	end
 
 
