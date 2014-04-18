5e8e940692214ce6c23041ade8eb06e9ad227b97
colas <colas.picard@laposte.net>
2014-04-18 15:24:34 +0200

    serie de tests de chaque classe

---
+++ Codes/Moteur/test3.rb
@@ -0,0 +1,44 @@
+#
+##Test des cases
+#
+
+load "Grille.rb"
+=begin
+my_case = Case.creer()
+my_case.noircir()
+my_case.afficher()
+my_case.marquer()
+my_case.afficher()
+my_case.raz()
+my_case.afficher()
+=end
+
+=begin
+puts "Test des rangees"
+
+c1 = Case.creer()
+c2 = Case.creer()
+c2.noircir()
+tab = Array.new()
+tab.push(c1)
+tab.push(c2)
+
+
+ra = Rangee.ajouter(2)
+ra.remplir(tab)
+#ra.afficher()
+ra.noircir(0)
+#ra.afficher()
+ra.conditionsDeterminer()
+ra.noircir(0)
+#ra.afficher()
+ra.marquer(0)
+puts ra.verifier().to_s()
+ra.afficher()
+
+=end
+
+puts "Test de la Grille"
+g1 = Grille.ajouter("toto", 5)
+g1.genererAleatoire(50)
+g1.afficher()
