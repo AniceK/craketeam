73265e92d0a3265655ace77f1116ee34932fcb20
AniceK <a.khomany@gmail.com>
2014-05-08 20:49:35 +0200

    Finalisation des grilles + début menu de pause

--- Codes/Controleur/EventsJeu.rb
+++ Codes/Controleur/EventsJeu.rb
@@ -13,12 +13,12 @@ require './Vue/Fenetres/FenetreJeu'
 
 class EventsJeu < Events
   
-  @taille
-  @imageCaseVide
-  @imageCaseNoircie
-  @imageCaseMarquee
+  @tailleGrille
+  @tailleCase
+  @nbConditionsRangee
+  @estEnPause
   
-  attr_reader :taille, :imageCaseVide, :imageCaseNoircie, :imageCaseMarquee
+  attr_reader :tailleGrille, :tailleCase, :nbConditionsRangee, :estEnPause
   
   public_class_method :new
   
@@ -26,24 +26,38 @@ class EventsJeu < Events
     
     super(jeu)
     
-    @taille = tailleGrille
-    @imageCaseVide = Gtk::Image.new('./Vue/Images/vide.gif')
-    @imageCaseNoircie = Gtk::Image.new('./Vue/Images/noircie.gif')
-    @imageCaseMarquee = Gtk::Image.new('./Vue/Images/marquee.gif')
+    @estEnPause = false
+    @tailleGrille = tailleGrille
+    @tailleCase = 30
+    @nbConditionsRangee = (@tailleGrille/2).round()
+    puts "@nbConditionsRangee = " + @nbConditionsRangee.to_s
     
     # Mise en place de la grille et de ses conditions
     
     @fenetre = FenetreJeu.new()
-    @fenetre.redimensionner(600 + (@taille*10), 350 + (@taille*10))
-    @fenetre.afficher()
     
-    initialiserGrille()
+    case @tailleGrille
+    when 5
+      initialiserGrille(500, 300)
+    when 10
+      initialiserGrille(700, 550)
+    when 15
+      initialiserGrille(700, 550)
+    when 20
+      initialiserGrille(850, 700)
+    when 25
+      initialiserGrille(1000, 800)
+    else
+      puts "Erreur: La taille n'est pas valide"
+    end
+    
     
     @fenetre.tempsEcoule = 0
     
     # Affichage
     
     @fenetre.afficher()
+    @fenetre.affichageJeu()
     
     @fenetre.boutonAide.signal_connect('clicked'){
       puts "> Aide"
@@ -52,14 +66,34 @@ class EventsJeu < Events
       mouvement(EventsPreparation.new(jeu)) # DEBUG
     }
     
-    @fenetre.boutonMenu.signal_connect('clicked'){
-      puts "> Menu"
+    @fenetre.boutonPause.signal_connect('clicked'){
       
-      #mouvement(EventsMenuJeu.new(jeu))
+      # Inversion du booléen
+      @estEnPause = @estEnPause.!
+
+      if @estEnPause then
+        @fenetre.affichagePause()
+      elsif !@estEnPause then
+        @fenetre.affichageJeu()
+      else
+        puts "Erreur du booléen \"@estEnPause\" déterminant la pause ou la lecture"
+      end
     }
   end
   
-  def initialiserGrille()
+  
+  
+  def initialiserGrille(largeurFenetre, hauteurFenetre)
+    
+    @fenetre.redimensionner(largeurFenetre, hauteurFenetre)
+    
+    if @tailleGrille < 15 then
+      @tailleCase = 30
+      @fenetre.grandesConditions()
+    else
+      @tailleCase = 20
+      @fenetre.petitesConditions()
+    end
     
     initialiserConditionsV()
     initialiserConditionsH()
@@ -67,41 +101,89 @@ class EventsJeu < Events
     
   end
   
+  
+  
+  # Retourne une case vide cliquable
+  def caseVide()
+    
+    pixCaseVide = Gdk::Pixbuf.new('./Vue/Images/vide.gif', @tailleCase, @tailleCase)
+    imageCaseVide = Gtk::Image.new(pixCaseVide)
+    eventCaseVide = Gtk::EventBox.new.add(imageCaseVide)
+    
+    eventCaseVide.signal_connect("button_press_event") do
+      puts "Case vide cliquée"
+    end
+    
+    return eventCaseVide
+  end
+  
+  # Retourne une case noircie cliquable
+  def caseNoircie()
+    
+    pixCaseNoircie = Gdk::Pixbuf.new('./Vue/Images/noircie.gif', @tailleCase, @tailleCase)
+    imageCaseNoircie = Gtk::Image.new(pixCaseNoircie)
+    eventCaseNoircie = Gtk::EventBox.new.add(imageCaseNoircie)
+    
+    eventCaseNoircie.signal_connect("button_press_event") do
+      puts "Case noircie cliquée"
+    end
+    
+    return eventCaseNoircie
+  end
+  
+  # Retourne une case marquée cliquable
+  def caseMarquee()
+    
+    pixCaseMarquee = Gdk::Pixbuf.new('./Vue/Images/marquee.gif', @tailleCase, @tailleCase)
+    imageCaseMarquee = Gtk::Image.new(pixCaseMarquee)
+    eventCaseMarquee = Gtk::EventBox.new.add(imageCaseMarquee)
+    
+    eventCaseMarquee.signal_connect("button_press_event") do
+      puts "Case marquée cliquée"
+    end
+    
+    return eventCaseMarquee
+  end
+  
   def initialiserConditionsV()
     
-    @fenetre.tableauConditionsV.resize(@taille, 3)
+    #@fenetre.tableauConditionsV.resize(@tailleGrille, 3)
     
-    0.upto(@taille) do |x|
-      0.upto(3) do |y|
-        @fenetre.tableauConditionsV.attach_defaults(Gtk::Label.new("0"), x, x+1, y, y+1)
+    1.upto(@tailleGrille) do |x|
+      1.upto(@nbConditionsRangee) do |y|
+        @fenetre.tableauConditionsV.attach(Gtk::Label.new("0"), x, x+1, y, y+1, Gtk::FILL | Gtk::SHRINK, Gtk::FILL | Gtk::SHRINK)
       end
     end
   end
   
   def initialiserConditionsH()
     
-    @fenetre.tableauConditionsH.resize(3, @taille)
+    #@fenetre.tableauConditionsH.resize(3, @tailleGrille)
     
-    0.upto(3) do |x|
-      0.upto(@taille) do |y|
-        @fenetre.tableauConditionsH.attach_defaults(Gtk::Label.new("0"), x, x+1, y, y+1)
+    1.upto(@nbConditionsRangee) do |x|
+      1.upto(@tailleGrille) do |y|
+        @fenetre.tableauConditionsH.attach(Gtk::Label.new("0"), x, x+1, y, y+1, Gtk::FILL | Gtk::SHRINK, Gtk::FILL | Gtk::SHRINK)
       end
     end
   end
   
   def initialiserTableauJeu()
     
-    @fenetre.tableauJeu.resize(@taille, @taille)
+    #@fenetre.tableauJeu.resize(@tailleGrille, @tailleGrille)
     
-    0.upto(@taille) do |x|
-      0.upto(@taille) do |y|
+    1.upto(@tailleGrille) do |x|
+      1.upto(@tailleGrille) do |y|
+        
+        # Ajout séparation toutes les 5 lignes
         
         #if (x%5 == 0) && (y%5 == 0) then
-        #  @fenetre.tableauJeu.attach_defaults(Gtk::HSeparator.new(), 0, @taille-1, y, y+1)
-        #  @fenetre.tableauJeu.attach_defaults(Gtk::HSeparator.new(), x, x+1, 0, @taille-1)
+        #  @fenetre.tableauJeu.attach_defaults(Gtk::HSeparator.new(), 0, @tailleGrille-1, y, y+1)
+        #  @fenetre.tableauJeu.attach_defaults(Gtk::HSeparator.new(), x, x+1, 0, @tailleGrille-1)
         #end
         
-        @fenetre.tableauJeu.attach_defaults(Gtk::Image.new('./Vue/Images/vide.gif'), x, x+1, y, y+1)
+        caseTableau = caseVide()
+        
+        @fenetre.tableauJeu.attach(caseTableau, x, x+1, y, y+1, Gtk::FILL | Gtk::SHRINK, Gtk::FILL | Gtk::SHRINK)
       end
     end
   end
--- Codes/Vue/Fenetres/FenetreJeu.rb
+++ Codes/Vue/Fenetres/FenetreJeu.rb
@@ -16,9 +16,11 @@ class FenetreJeu < Fenetre
   @tableauConditionsH
   @tableauJeu
   @boutonAide
-  @boutonMenu
+  @boutonPause
+  @imagePause
+  @imageLecture
   
-  attr_reader :texteTemps, :boutonAide, :boutonMenu
+  attr_reader :texteTemps, :boutonAide, :boutonPause, :imagePause, :imageLecture
   attr_accessor :tableauConditionsV, :tableauConditionsH, :tableauJeu, :tempsEcoule
   
   public_class_method :new
@@ -28,45 +30,118 @@ class FenetreJeu < Fenetre
     
     @fenetre.set_title("Jouez !")
     
+    #@menuPause = MenuPause.new()
+    @menuPause = Gtk::Label.new("Menu de Pause")
+    
     @texteTemps = Gtk::Label.new("Temps écoulé :")
     @tempsEcoule = Gtk::Label.new("0")
     @boutonAide = Gtk::Button.new('Aide')
-    @boutonMenu = Gtk::Button.new('Menu')
+    @boutonPause = Gtk::Button.new()
     @tableauGeneral = Gtk::Table.new(0, 0, false)
-    @tableauConditionsV = Gtk::Table.new(0, 0, false)
-    @tableauConditionsH = Gtk::Table.new(0, 0, false)
-    @tableauJeu = Gtk::Table.new(0, 0, false)
+    @tableauConditionsV = Gtk::Table.new(0, 0, true)
+    @tableauConditionsH = Gtk::Table.new(0, 0, true)
+    @tableauJeu = Gtk::Table.new(0, 0, true)
+    tableauDefile = Gtk::ScrolledWindow.new
+    pixPause = Gdk::Pixbuf.new('./Vue/Images/pause.gif')
+    @imagePause = Gtk::Image.new(pixPause)
+    pixLecture = Gdk::Pixbuf.new('./Vue/Images/lecture.gif')
+    @imageLecture = Gtk::Image.new(pixLecture)
     
     @boutonAide.set_size_request(80, 30)
-    @boutonMenu.set_size_request(80, 30)
-    @tableauGeneral.attach_defaults(@tableauConditionsV, 1, 2, 0, 1)
-    @tableauGeneral.attach_defaults(@tableauConditionsH, 0, 1, 1, 2)
+  
+    @boutonPause.set_image(imagePause)
+    
+    @tableauConditionsH.set_column_spacings(5)
+    
+    @tableauJeu.set_column_spacings(2)
+    @tableauJeu.set_row_spacings(2)
+    
+    @alignePause = Gtk::Alignment.new(0.5,0,0,0)
+    @alignePause.add(@boutonPause)
+    
+    @aligneConditionsH = Gtk::Alignment.new(1,0,0,1)
+    @aligneConditionsH.set_padding(12, 0, 0, 0)
+    @aligneConditionsH.add(@tableauConditionsH)
+    
+    @aligneConditionsV = Gtk::Alignment.new(0,1,1,0)
+    @aligneConditionsV.set_padding(0, 0, 10, 0)
+    @aligneConditionsV.add(@tableauConditionsV)
+    
+    @tableauGeneral.attach_defaults(@aligneConditionsV, 1, 2, 0, 1)
+    @tableauGeneral.attach_defaults(@aligneConditionsH, 0, 1, 1, 2)
     @tableauGeneral.attach_defaults(@tableauJeu, 1, 2, 1, 2)
     
-    hBoxTemps = Gtk::HBox.new(true, 0)
-    hBoxBoutons = Gtk::HBox.new(true, 0)
-    vBox = Gtk::VBox.new(false, 0)
+    hBoxTemps = Gtk::HBox.new(false, 0)
+    vBoxDroite = Gtk::VBox.new(true, 0)
+    @hBoxPrincipale = Gtk::HBox.new(false, 5)
     
     hBoxTemps.set_border_width(5)
-    hBoxBoutons.set_border_width(5)
-    vBox.set_border_width(5)
+    vBoxDroite.set_border_width(5)
+    @hBoxPrincipale.set_border_width(5)
     
     hBoxTemps.pack_start(@texteTemps, false, false, 0)
-    hBoxTemps.pack_start(@tempsEcoule, false, false, 0)
+    hBoxTemps.pack_start(@tempsEcoule, false, false, 20)
     
-    hBoxBoutons.pack_start(@boutonAide, false, false, 0)
-    hBoxBoutons.pack_start(@boutonMenu, false, false, 0)
+    vBoxDroite.pack_start(hBoxTemps, false, false, 30)
+    vBoxDroite.pack_start(@alignePause, false, false, 30)
+    vBoxDroite.pack_end(@boutonAide, false, false, 30)
     
-    vBox.pack_start(hBoxTemps, false, false, 10)
-    vBox.pack_start(Gtk::HSeparator.new(), false, false, 0)
-    vBox.pack_start(@tableauGeneral, false, false, 0)
-    vBox.pack_start(Gtk::HSeparator.new(), false, false, 0)
-    vBox.pack_start(hBoxBoutons, false, false, 10)
+    @hBoxPrincipale.pack_start(@menuPause, true, true, 0)  # DEBUG
+    @hBoxPrincipale.pack_start(@tableauGeneral, true, true, 0)
+    @hBoxPrincipale.pack_end(vBoxDroite, false, false, 10)
+    @hBoxPrincipale.pack_end(Gtk::VSeparator.new(), false, false, 0)
     
-    @fenetre.add(vBox)
+    @fenetre.add(@hBoxPrincipale)
+    
+  end
+  
+  def grandesConditions
+    
+    @tableauConditionsV.set_column_spacings(25)
+    @tableauConditionsH.set_row_spacings(19)
+  end
+  
+  def petitesConditions
+    
+    @aligneConditionsH.set_padding(0, 0, 0, 0)
+    @tableauConditionsV.set_column_spacings(15)
+    
+    @aligneConditionsH.set_padding(5, 0, 0, 0)
+    @tableauConditionsH.set_row_spacings(9.3)
+  end
+
+  
+  
+  def affichagePause()
+    
+    puts "> Pause"
+    @boutonPause.set_image(imageLecture)
+    
+    @tableauGeneral.hide_all()   
+    @menuPause.show_all()
+  end
+  
+  def affichageJeu()
+    
+    puts "> Lecture"
+    @boutonPause.set_image(imagePause)
+    
+    @menuPause.hide_all()
+    @tableauGeneral.show_all()
   end
   
 end
+
+
+
+
+
+
+
+
+
+
+
 
 
 
