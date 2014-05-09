12b55e41cff052d0cac78a723dfe78fb2766cb18
AniceK <a.khomany@gmail.com>
2014-05-09 11:00:39 +0200

    MAJ liens fichiers

--- Codes/Test/test3.rb
+++ Codes/Test/test3.rb
@@ -2,9 +2,9 @@
 ##Test des cases
 #
 
-load "Grille.rb"
-load "Aide.rb"
-load "Rangee.rb"
+require "../Modele/Grille.rb"
+require "../Modele/Aide.rb"
+require "../Modele/Rangee.rb"
 =begin
 my_case = Case.creer()
 my_case.noircir()
