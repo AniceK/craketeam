f0dbb096c5cd3a15d02f42aab5f6b604e64b8fbd
AniceK <a.khomany@gmail.com>
2014-05-09 15:18:53 +0200

    Inversion taille/nom dans méthode partie

--- Codes/Modele/Partie.rb
+++ Codes/Modele/Partie.rb
@@ -35,7 +35,7 @@ class Partie
 #méthode de classe d'initialisation prenant en paramètre la taille de la grille, le niveau de difficulté de l'aide et le nom du joueur
     def initialize(taille, difficulte, nom)
 
-		@grille = Grille.creer(taille, nom)
+		@grille = Grille.creer(nom, taille)
 		@creation = Time.now
         @joueur = nom
         @aide = Aide.creer(difficulte)
